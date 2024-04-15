96e6a11e3d2f1eb5929f6a0013934ad9941f3429
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-15 10:49:51 +0100

    extend your Python script to export data in the CSV format

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script will use the REST API to return information
+about a given employee's TODO list progress.
+"""
+import requests
+import urllib.request
+import sys
+import json
+import pandas as pd
+
+
+def TODO_REQUESTS(ID):
+    """
+    extend your Python script to export data in the CSV format
+    """
+    todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={ID}"
+    ).json()
+    user_info = requests.get(
+        f"https://jsonplaceholder.\
+typicode.com/users/{ID}"
+    ).json()
+    df = pd.DataFrame(todos)
+    df['username'] = user_info['username']
+    df = df[['userId', 'username', 'completed', 'title']]
+    df.to_csv(f"{ID}.csv", index=False)
+
+
+if __name__ == "__main__":
+    ID = sys.argv[1]
+    TODO_REQUESTS(ID)
