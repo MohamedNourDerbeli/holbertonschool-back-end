4225d36661a5785ce97b58c8bba1209ca6a45e58
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-16 01:43:09 +0100

    Dictionary of list of dictionaries

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This script will use the REST API to return information
+about a given employee's TODO list progress.
+"""
+import json
+import requests
+
+def TODO_REQUESTS():
+    """
+    extend your Python script to export data in the JSON format
+    """
+    todos = requests.get(f"https://jsonplaceholder.typicode.com/todos").json()
+    users = requests.get(
+        f"https://jsonplaceholder.\
+typicode.com/users"
+    ).json()
+
+    data = [
+        {
+            user["id"]: [
+                {
+                    "username": user["username"],
+                    "task": task["title"],
+                    "completed": task["completed"],
+                }
+                for task in todos
+            ]
+        }
+        for user in users
+    ]
+    with open(f"todo_all_employees.json", "w") as jsonfile:
+        json.dump(data[0], jsonfile)
+
+
+if __name__ == "__main__":
+    TODO_REQUESTS()
