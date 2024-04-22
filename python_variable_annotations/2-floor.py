2cbc71277cc7f2be8ee60bca0f4ca8c5831aad25
Mohamed Nour <89344530+MohamedNourDerbeli@users.noreply.github.com>
2024-04-22 06:25:28 +0100

    Delete python_variable_annotations directory

--- python_variable_annotations/2-floor.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""function floor which takes a float n"""
-
-
-def floor(n: float) -> int:
-    """return the floor of the float"""
-    return int(n)
--- python_variable_annotations/3-to_str.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Return a string representation of the float n.
-"""
-
-
-def to_str(n: float) -> str:
-    """return str(n)"""
-    return str(n)
